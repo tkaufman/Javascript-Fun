2b7b8a0b773ffa7fe38720d2cea8c861b7033673
Todd Kaufman <toddkaufman@gmail.com>
2012-07-06 08:44:12 -0400

    Updated to a rails app in order to use jasmine-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JavascriptFun::Application.config.secret_token = '31ce6ff0d6dd390b86d5e39c7bb49d576f478198172afa0d31f373c6cdb1007da8bab6474148400a787a59fcc5048b4f2edfcfc72c63c7299d5209adb93b06fa'
